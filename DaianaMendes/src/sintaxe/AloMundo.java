2b09f0b0a2037e5488c1f98e25affb2071485471
Daiana Mendes <103112318+daianamendesss@users.noreply.github.com>
2023-02-08 08:20:23 -0300

    Add files via upload

---
+++ DaianaMendes/src/sintaxe/AloMundo.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package sintaxe;
+
+/**
+ *
+ * @author 15385404608
+ */
+public class AloMundo {
+    public static void main(String[] args){
+    System.out.println("Alô Mundo...");
+    String ano = args[0];
+    String senha = args[1];
+    String nome = args[2];
+    
+    System.out.println("Ano: "+ano);
+    System.out.println("Senha: "+senha);  
+    System.out.println("Nome: "+nome);
+    
+    
+    
+ 
+    }
+    
+}
